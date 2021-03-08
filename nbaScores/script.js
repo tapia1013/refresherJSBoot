cbfd2adbfab79771f04e5c4ab6f3992f51140729
tapia1013 <manbearpig1013@gmail.com>
2021-03-08 14:05:49 -0800

    refactored

--- nbaScores/script.js
+++ nbaScores/script.js
@@ -85,59 +85,165 @@ const warriorsGames = [{
 ]
 
 
-// make new element container
-const ulparent = document.createElement('ul')
-
-// iterate over the games
-for (let game of warriorsGames) {
-  const { homeTeam, awayTeam } = game
-
-  // console.log(awayTeam.team, homeTeam.team);
-
-  // we want an li for each team
-  const gameLi = document.createElement('li');
-
-  // destructure points and team out of object
-  const {
-    team: hTeam,
-    points: hPoints
-  } = homeTeam;
-
-  const {
-    team: aTeam,
-    points: aPoints
-  } = awayTeam
 
 
-  // GSW @ Houston
-  // gameLi.innerText = `${awayTeam.team} @ ${homeTeam.team}`;
+
+
+
+// **********************************************
+//         NON REFACTORED CODE
+// *********************************************
+
+// // make new element container
+// const ulparent = document.createElement('ul')
+
+// // iterate over the games
+// for (let game of warriorsGames) {
+//   const { homeTeam, awayTeam } = game
+
+//   // console.log(awayTeam.team, homeTeam.team);
+
+//   // we want an li for each team
+//   const gameLi = document.createElement('li');
+
+//   // destructure points and team out of object
+//   const {
+//     team: hTeam,
+//     points: hPoints
+//   } = homeTeam;
+
+//   const {
+//     team: aTeam,
+//     points: aPoints
+//   } = awayTeam
+
+
+//   // GSW @ Houston
+//   // gameLi.innerText = `${awayTeam.team} @ ${homeTeam.team}`;
+//   const teamNames = `${aTeam} @ ${hTeam}`
+
+//   // have to put scoreline here cause if statement doesnt have access to it cause of scope
+//   let scoreLine;
+
+//   // compare the 2 points
+//   if (aPoints > hPoints) {
+//     // score - score
+//     scoreLine = `<b>${aPoints}</b>-${hPoints}`
+//   } else {
+//     scoreLine = `${aPoints}-<b>${hPoints}</b>`
+//   }
+
+//   // check if gsw
+//   const warriors = hTeam === 'Golden State' ? homeTeam : awayTeam
+//   // is gsw winner?
+//   gameLi.classList.add(warriors.isWinner ? 'win' : 'loss')
+
+//   gameLi.innerHTML = `${teamNames} ${scoreLine}`
+
+
+//   // append each li into the ul
+//   ulparent.appendChild(gameLi)
+// }
+
+// // For it to show up we need to append it to the body, shows "Golden State @ Houston" now
+// document.body.prepend(ulparent)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ************************************************
+//                REFACTORED CODE REUSABLE
+// ************************************************
+
+const makeChart = (games, targetTeam) => {
+  const ulParent = document.createElement('ul')
+  for (let game of games) {
+    const gameLi = document.createElement('li')
+    gameLi.innerHTML = getScoreLine(game)
+    // we can move this to add a closs of win or loss
+    gameLi.classList.add(isWinner(game, targetTeam) ? 'win' : 'loss')
+    ulParent.appendChild(gameLi)
+  }
+  return ulParent
+}
+
+const isWinner = ({ homeTeam, awayTeam }, targetTeam) => {
+  const target = homeTeam.team === targetTeam ? homeTeam : awayTeam
+  return target.isWinner
+}
+
+const getScoreLine = ({ homeTeam, awayTeam }) => {
+  const { team: hTeam, points: hPoints } = homeTeam
+  const { team: aTeam, points: aPoints } = awayTeam
   const teamNames = `${aTeam} @ ${hTeam}`
-
-  // have to put scoreline here cause if statement doesnt have access to it cause of scope
   let scoreLine;
-
-  // compare the 2 points
   if (aPoints > hPoints) {
-    // score - score
     scoreLine = `<b>${aPoints}</b>-${hPoints}`
   } else {
     scoreLine = `${aPoints}-<b>${hPoints}</b>`
   }
-
-  // check if gsw
-  const warriors = hTeam === 'Golden State' ? homeTeam : awayTeam
-  // is winner?
-  gameLi.classList.add(warriors.isWinner ? 'win' : 'loss')
-
-  gameLi.innerHTML = `${teamNames} ${scoreLine}`
-
-
-  // append each li into the ul
-  ulparent.appendChild(gameLi)
+  return `${teamNames} ${scoreLine}`
 }
 
-// For it to show up we need to append it to the body, shows "Golden State @ Houston" now
-document.body.prepend(ulparent)
+// select the sections in html
+const gswSection = document.getElementById('gsw')
+const hrSection = document.getElementById('hr')
+
+// second param gets green background
+const gswChart = makeChart(warriorsGames, 'Golden State')
+const hrChart = makeChart(warriorsGames, 'Houston')
+
+gswSection.appendChild(gswChart)
+hrSection.appendChild(hrChart)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
